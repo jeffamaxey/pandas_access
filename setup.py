ce30c33b0a51e0d79ab65510d8d92201a866aabb
Frank Fishburn <48447032+ffishburn@users.noreply.github.com>
2019-10-03 13:48:38 -0500

    Update setup.py to allow building eggs (#5)

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 
 README_FILE = os.path.join(os.path.dirname(__file__), 'README.md')
@@ -7,7 +7,7 @@ README_FILE = os.path.join(os.path.dirname(__file__), 'README.md')
 setup(
     name="pandas_access",
     version="0.0.1",
-    packages=["pandas_access"], # Basically, reserve that namespace.
+    packages=find_packages(),
     license="License :: OSI Approved :: MIT License",
     author="John Bjorn Nelson",
     author_email="jbn@abreka.com",
